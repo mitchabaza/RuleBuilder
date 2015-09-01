be732b94118200e01d170aa519cadf77a78800e9
mitchabaza <mitch.abaza@gmail.com>
2015-09-01 13:23:16 -0500

    s

---
+++ Client/worklist/actions/Undo.js
@@ -0,0 +1,11 @@
+﻿var AppDispatcher = require('../dispatcher/AppDispatcher');
+var Constants = require('../constants/RuleActionConstants');
+var uuid = require("uuid");
+// Define action methods
+var Actions = {
+    fire: function () {
+
+        AppDispatcher.dispatchUndo();
+    }
+};
+module.exports = Actions;
